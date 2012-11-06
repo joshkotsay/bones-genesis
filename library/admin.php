70fe188bd14f44d7f17b45d9608df889ef98020f
Kevin Salter <kevinsalterdesign@gmail.com>
2012-11-06 13:57:29 -0800

    change bones_login_css to use login_enqueue_scripts

--- library/admin.php
+++ library/admin.php
@@ -92,10 +92,15 @@ add_action('wp_dashboard_setup', 'bones_custom_dashboard_widgets');
 /************* CUSTOM LOGIN PAGE *****************/
 
 // calling your own login css so you can style it
-function bones_login_css() {
-	/* i couldn't get wp_enqueue_style to work :( */
-	echo '<link rel="stylesheet" href="'. get_stylesheet_directory_uri() . '/library/css/login.css">';
-}
+// function bones_login_css() {
+// 	/* i couldn't get wp_enqueue_style to work :( */
+// 	echo '<link rel="stylesheet" href="'. get_stylesheet_directory_uri() . '/library/css/login.css">';
+// }
+
+//grabbed this code from http://codex.wordpress.org/Customizing_the_Login_Form#Styling_Your_Login
+function bones_login_css() { ?>
+    <link rel="stylesheet" id="custom_wp_admin_css"  href="<?php echo get_stylesheet_directory_uri() . '/library/css/login.css'; ?>" type="text/css" media="all" />
+<?php }
 
 // changing the logo link from wordpress.org to your site
 function bones_login_url() { return get_bloginfo('url'); }
@@ -104,7 +109,7 @@ function bones_login_url() { return get_bloginfo('url'); }
 function bones_login_title() { return get_option('blogname'); }
 
 // calling it only on the login page
-add_action('login_head', 'bones_login_css');
+add_action( 'login_enqueue_scripts', 'bones_login_css' );
 add_filter('login_headerurl', 'bones_login_url');
 add_filter('login_headertitle', 'bones_login_title');
 
